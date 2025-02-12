425a908559104396cf300f69b4abfe03fecc6921
ThallahAryo <marcelloarvinalsaki@gmail.com>
2025-02-12 14:19:30 +0700

    Add files via upload

---
+++ bintangkanan.dart
@@ -0,0 +1,6 @@
+void main() {
+  int n = 10;
+  for (var i = 1; i <= n; i++) {
+    print(' ' * (n - i) + '*' * i);
+  }
+}
---
+++ helloworld.dart
@@ -0,0 +1,4 @@
+void main() {
+  var name = "thallah anindia aryo putri";
+  print(name);
+}
